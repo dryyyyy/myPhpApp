d03c28a0d820a23b730b9cd75bbcb59b40d4b78a
Dima <xdryx@ngs.ru>
2018-06-18 12:14:25 +0000

    minor fix, characters which aren't in a-z A-Z 0-9 set or are not spaces are not going to be counted as words anymore

--- class.wordCounter.php
+++ class.wordCounter.php
@@ -5,10 +5,10 @@ class WordCounter{
     
     public function count($file_contents){
         if(!is_string($file_contents)){
-            throw new Exception ('WordCounter->count(): $file_contents must be a sting');
+            throw new Exception ('WordCounter->count(): expects a sting as an attribute');
         } else {
-            //puts an array of words as $keys and number they are met in the text as $values
-            $this->words = (array_count_values(str_word_count(strtolower($file_contents), 1)));
+            $file_contents = preg_replace("/[^A-Za-z0-9 ]/", '', $file_contents); // removing all non-alphanumeric characters as they are not to be counted as words
+            $this->words = (array_count_values(str_word_count(strtolower($file_contents), 1))); // puts an array of words as $keys and number they are met in the text as $values
             return $this->words;
         }
         
